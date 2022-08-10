2e1e484bdb64fec6a6db3b8ae12d28f065b99559
dfragnito <dean@xornet.com>
2022-08-10 07:58:46 -0400

    Update shopadmin.js
    created shop roots for multiple shops removed delte and purge

--- netlify/functions/shopadmin.js
+++ netlify/functions/shopadmin.js
@@ -164,10 +164,191 @@ function randomString(length, chars) {
     return result;
 }
 
-const root = randomString(12, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-	 
-const sfsqlReqPayload = `[{"delete":{"objfilter":"SELECT $o:${root}.attrset('delete')"}},{"purge":{}},{"modify":{"data":{"${root}":{"SHOP":{"INV":{"supplier${root}":{"name":"Sony Group Corporation","id":"sony"},"product${root}":[{"name":"FinePix Pro2 3D Camera","code":"3DcAM01","image":"camera.jpg","n:price":300.0,"i:totalordered":0,"manufacturer${root}":{"#ref":"SELECT $o:.supplier${root}.oid() WHERE $s:.supplier${root}.id='sony'"}},{"name":"EXP Portable Hard Drive","code":"USB02","image":"external-hard-drive.jpg","n:price":800.0,"i:totalordered":0,"manufacturer${root}":{"#ref":"SELECT $o:.supplier${root}.oid() WHERE $s:.supplier${root}.id='sony'"}},{"name":"Luxury Ultra thin Wrist Watch","code":"wristWear03","image":"watch.jpg","n:price":100.0,"i:totalordered":0,"manufacturer${root}":{"name":"TAG Heuer","id":"tag"}},{"name":"XP 1155 Intel Core Laptop","code":"LPN45","image":"laptop.jpg","n:price":250.0,"i:totalordered":0,"manufacturer${root}":{"name":"DELL Inc","id":"dell"}}],"o:supplier${root}":[{"#append":{}},{"#ref":"SELECT $o:.manufacturer${root}.oid() WHERE $s:.manufacturer${root}.id='tag'"},{"#ref":"SELECT $o:.manufacturer${root}.oid() WHERE $s:.manufacturer${root}.id='dell'"}]},"customer${root}":{"#set":{"where":"$s:customer${root}.id='c1111'"},"s:id":"c1111","s:first_name":"Larry","s:last_name":"Smith","o:address":{"street":"5 Elmwood Avenue","city":"Rochester","state":"NY","zip":"14616"}},"o:order${root}":[{"#append":{}},{"i:order_id":1,"d:datetime":"now","o:cust${root}":{"#ref":"SELECT $o:.customer${root}.oid() WHERE $s:.customer${root}.id='c1111'","email":"support@schemafreesql.com"},"o:lineitem":[{"o:item":{"#ref":"SELECT $o:.product${root}.oid() WHERE $s:.product${root}.code='3DcAM01'","i:totalordered":{"#pass":"v + 1"}},"i:no":1,"i:qty":1,"n:price":300.0},{"o:item":{"#ref":"SELECT $o:.product${root}.oid() WHERE $s:.product${root}.code='wristWear03'","i:totalordered":{"#pass":"v + 2"}},"i:no":2,"i:qty":2,"n:price":100.0}]},{"i:order_id":2,"d:datetime":"now","o:cust${root}":{"s:id":"c2222","s:first_name":"Sally","s:last_name":"Swanson","email":"feedback@schemafreesql.com","o:address":{"street":"7 Broadway","city":"New York","state":"NY","zip":"10003"}},"o:lineitem":[{"o:item":{"#ref":"SELECT $o:.product${root}.oid() WHERE $s:.product${root}.code='LPN45'","i:totalordered":{"#pass":"v + 2"}},"i:no":1,"i:qty":2,"n:price":250.0}]}],"o:customer${root}":[{"#append":{}},{"#ref":"SELECT $o:.cust${root}.oid() WHERE $s:.cust${root}.id='c2222'"}]}}}}},{"query":{"sfsql":"SELECT $s:.supplier${root}.name as name, $s:.supplier${root}.id as id","_q":"supp"}},{"query":{"sfsql":"SELECT $s:.product${root}.code as code,$s:.product${root}.name as name,$s:.product${root}.image as image,$n:.product${root}.price as price,$i:.product${root}.totalordered as totordered,$s:.product${root}.manufacturer${root}.name as manname","_q":"prod"}},{"query":{"sfsql":"SELECT $s:.customer${root}.id as cusid,$s:.customer${root}.first_name as fname,$s:.customer${root}.last_name as lname,$s:.customer${root}.email as email,$s:.customer${root}.address.street as street,$s:.customer${root}.address.city as city,$s:.customer${root}.address.state as state,$s:.customer${root}.address.zip as zip","_q":"cust"}},{"query":{"sfsql":"SELECT $i:.order${root}.order_id as orderid, $s:.order${root}.lineitem.item.code as code,$s:.order${root}.lineitem.item.name as name,$s:.order${root}.lineitem.item.manufacturer${root}.name as manname, $s:.order${root}.lineitem.item.image as image, $i:.order${root}.lineitem.no as no, $i:.order${root}.lineitem.qty as quantity, $n:.order${root}.lineitem.price as price, $s:.order${root}.cust${root}.id as cusid,$s:.order${root}.cust${root}.first_name as fname, $s:.order${root}.cust${root}.last_name as lname,$s:.order${root}.cust${root}.email as email,$s:.order${root}.cust${root}.address.street as street, $s:.order${root}.cust${root}.address.city as city, $s:.order${root}.cust${root}.address.state as state,  $s:.order${root}.cust${root}.address.zip as zip","_q":"order"}},{"delete":{"objfilter":"SELECT $o:${root}.attrset('delete')"}},{"purge":{}}]`;
-	 
+const root = 'ntshop' + randomString(12, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+const sfsqlReqPayload=`[
+	{
+		"modify": {
+			"data": {
+				"shops": {
+					"${root}": {
+						"supplier": {
+							"name": "Sony Group Corporation",
+							"id": "sony"
+						},
+						"product": [
+							{
+								"name": "FinePix Pro2 3D Camera",
+								"code": "3DcAM01",
+								"image": "camera.jpg",
+								"n:price": 300.0,
+								"i:totalordered": 0,
+								"manufacturer": {
+									"#ref": "SELECT $o:.${root}.supplier.oid() WHERE $s:.${root}.supplier.id='sony'"
+								}
+							},
+							{
+								"name": "EXP Portable Hard Drive",
+								"code": "USB02",
+								"image": "external-hard-drive.jpg",
+								"n:price": 800.0,
+								"i:totalordered": 0,
+								"manufacturer": {
+									"#ref": "SELECT $o:.${root}.supplier.oid() WHERE $s:.${root}.supplier.id='sony'"
+								}
+							},
+							{
+								"name": "Luxury Ultra thin Wrist Watch",
+								"code": "wristWear03",
+								"image": "watch.jpg",
+								"n:price": 100.0,
+								"i:totalordered": 0,
+								"manufacturer": {
+									"name": "TAG Heuer",
+									"id": "tag"
+								}
+							},
+							{
+								"name": "XP 1155 Intel Core Laptop",
+								"code": "LPN45",
+								"image": "laptop.jpg",
+								"n:price": 250.0,
+								"i:totalordered": 0,
+								"manufacturer": {
+									"name": "DELL Inc",
+									"id": "dell"
+								}
+							}
+						],
+						"o:supplier": [
+							{
+								"#append": {}
+							},
+							{
+								"#ref": "SELECT $o:.${root}.product.manufacturer.oid() WHERE $s:.${root}.product.manufacturer.id='tag'"
+							},
+							{
+								"#ref": "SELECT $o:.${root}.product.manufacturer.oid() WHERE $s:.${root}.product.manufacturer.id='dell'"
+							}
+						],
+						"customer": {
+							"#set": {
+								"where": "$s:customer.id='c1111'"
+							},
+							"s:id": "c1111",
+							"s:first_name": "Larry",
+							"s:last_name": "Smith",
+							"o:address": {
+								"street": "5 Elmwood Avenue",
+								"city": "Rochester",
+								"state": "NY",
+								"zip": "14616"
+							}
+						},
+						"o:order": [
+							{
+								"#append": {}
+							},
+							{
+								"i:order_id": 1,
+								"d:datetime": "now",
+								"o:cust": {
+									"#ref": "SELECT $o:.${root}.customer.oid() WHERE $s:.${root}.customer.id='c1111'",
+									"email": "support@schemafreesql.com"
+								},
+								"o:lineitem": [
+									{
+										"o:item": {
+											"#ref": "SELECT $o:.${root}.product.oid() WHERE $s:.${root}.product.code='3DcAM01'",
+											"i:totalordered": {
+												"#pass": "v + 1"
+											}
+										},
+										"i:no": 1,
+										"i:qty": 1,
+										"n:price": 300.0
+									},
+									{
+										"o:item": {
+											"#ref": "SELECT $o:.${root}.product.oid() WHERE $s:.${root}.product.code='wristWear03'",
+											"i:totalordered": {
+												"#pass": "v + 2"
+											}
+										},
+										"i:no": 2,
+										"i:qty": 2,
+										"n:price": 100.0
+									}
+								]
+							},
+							{
+								"i:order_id": 2,
+								"d:datetime": "now",
+								"o:cust": {
+									"s:id": "c2222",
+									"s:first_name": "Sally",
+									"s:last_name": "Swanson",
+									"email": "feedback@schemafreesql.com",
+									"o:address": {
+										"street": "7 Broadway",
+										"city": "New York",
+										"state": "NY",
+										"zip": "10003"
+									}
+								},
+								"o:lineitem": [
+									{
+										"o:item": {
+											"#ref": "SELECT $o:.${root}.product.oid() WHERE $s:.${root}.product.code='LPN45'",
+											"i:totalordered": {
+												"#pass": "v + 2"
+											}
+										},
+										"i:no": 1,
+										"i:qty": 2,
+										"n:price": 250.0
+									}
+								]
+							}
+						],
+						"o:customer": [
+							{
+								"#append": {}
+							},
+							{
+								"#ref": "SELECT $o:.${root}.order.cust.oid() WHERE $s:.${root}.order.cust.id='c2222'"
+							}
+						]
+					}
+				}
+			}
+		}
+	},
+	{
+		"query": {
+			"sfsql": "SELECT $s:.${root}.supplier.name as name, $s:.${root}.supplier.id as id",
+			"_q": "supp"
+		}
+	},
+	{
+		"query": {
+			"sfsql": "SELECT $s:.${root}.product.code as code,$s:.${root}.product.name as name,$s:.${root}.product.image as image,$n:.${root}.product.price as price,$i:.${root}.product.totalordered as totordered,$s:.${root}.product.manufacturer.name as manname",
+			"_q": "prod"
+		}
+	},
+	{
+		"query": {
+			"sfsql": "SELECT $s:.${root}.customer.id as cusid,$s:.${root}.customer.first_name as fname,$s:.${root}.customer.last_name as lname,$s:.${root}.customer.email as email,$s:.${root}.customer.address.street as street,$s:.${root}.customer.address.city as city,$s:.${root}.customer.address.state as state,$s:.${root}.customer.address.zip as zip",
+			"_q": "cust"
+		}
+	},
+	{
+		"query": {
+			"sfsql": "SELECT $i:.${root}.order.order_id as orderid, $s:.${root}.order.lineitem.item.code as code,$s:.${root}.order.lineitem.item.name as name,$s:.${root}.order.lineitem.item.manufacturer.name as manname, $s:.${root}.order.lineitem.item.image as image, $i:.${root}.order.lineitem.no as no, $i:.${root}.order.lineitem.qty as quantity, $n:.${root}.order.lineitem.price as price, $s:.${root}.order.cust.id as cusid,$s:.${root}.order.cust.first_name as fname, $s:.${root}.order.cust.last_name as lname,$s:.${root}.order.cust.email as email,$s:.${root}.order.cust.address.street as street, $s:.${root}.order.cust.address.city as city, $s:.${root}.order.cust.address.state as state,  $s:.${root}.order.cust.address.zip as zip",
+			"_q": "order"
+		}
+	}
+]`;
+
 const fetch = require("node-fetch");
 
 const API_ENDPOINT = process.env.endpoint;
