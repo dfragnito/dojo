4fef0cd99db07aa1ef3be7fadcd08bf33ebca40e
dfragnito <dean@xornet.com>
2022-08-23 16:58:58 -0400

    update

--- netlify/functions/got.js
+++ netlify/functions/got.js
@@ -28,10 +28,49 @@ exports.handler = async (event, context) => {
         };
         const response = await fetch(API_ENDPOINT, init);
         const data =  await response.json();
-		  const nodes = data[0]['data'];
+		  //const nodes = data[0]['data'];
+		  
+		  let str1 = ''; 
+
+			let i = 0;
+			while (i < data[0]['data'].length) {
+
+			  str1 += `{
+					"data":{
+						"id":"${data[0]['data'][i]["id"]}",
+						"label":"${data[0]['data'][i]['label']}" 
+						}
+						 },`
+			  i++;
+
+			} 
+
+			let str2 = ''; 
+			 
+			let j = 0;
+			while (j < data[1]['data'].length) {
+
+			  str2 += `{
+					"data":{
+						"id":"${data[1]['data'][j]['id']}",
+						"source":"${data[1]['data'][j]['source']}",
+                  "target":"${data[1]['data'][j]['target']}" 						
+						}
+						 },`
+			  i++;
+
+			}
+
+			const newstr2 = str2.slice(0, -1);
+			
+			const str3 = str1.concat(newstr2);
+		  
+		  
+		  
+		  
 		  return {
 				statusCode: 200,
-				body: JSON.stringify(nodes),
+				body: JSON.stringify(str3),
 			 }; 
 		
 };	 
