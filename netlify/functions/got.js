08c0182507b573277e5e24670a308a804d7f1ec3
dfragnito <dean@xornet.com>
2022-08-23 13:23:09 -0400

    moved code to edge function

--- netlify/functions/got.js
+++ netlify/functions/got.js
@@ -28,8 +28,46 @@ const sfsqlReqPayload=`[
 	  })
     .then((response) => response.json())
     .then((data) => ({
+		  var data = JSON.parse(data)
+		  
+		  let str1 = '' 
+
+			let i = 0;
+			while (i < data[0]["data"].length) {
+
+			  str1 += `{
+					"data":{
+						"id":"${data[0]['data'][i]['id']}",
+						"label":"${data[0]['data'][i]['label']}" 
+						}
+						 },`
+			  i++;
+
+			}
+
+			//let newstr1 = str1.slice(0, -1);
+			 
+			let str2 = ''
+			let j = 0;
+			while (j < data[1]["data"].length) {
+
+			  str2 += `{
+					"data":{
+						"id":"${data[1]['data'][j]['id']}",
+						"source":"${data[1]['data'][j]['source']}",
+						"target":"${data[1]['data'][j]['target']}"			
+						}
+						 },`
+			  i++;
+
+			}
+
+         let newstr2 = str2.slice(0, -1);
+		 
+	
+		 
       statusCode: 200,
-      body:JSON.stringify(data),
+      body:JSON.stringify(newstr2),
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };	 
