e8867cbea3d941598d5f355fd5278339e4e23a1d
dfragnito <dean@xornet.com>
2022-08-23 18:15:44 -0400

    element

--- netlify/functions/nodes.js
+++ netlify/functions/nodes.js
@@ -39,10 +39,10 @@ exports.handler = async (event, context) => {
 
 			}
 	
-		  
+		   const newstr1 = str1.slice(0, -1);
 		  return {
 				statusCode: 200,
-				body: JSON.stringify(str1),
+				body: JSON.stringify(newstr1),
 			 }; 
 		
 };	 
