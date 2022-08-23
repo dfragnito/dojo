404b6274b89810a7cddca046fe9b5d18d01fd070
dfragnito <dean@xornet.com>
2022-08-23 15:04:15 -0400

    new fetch

--- netlify/functions/got.js
+++ netlify/functions/got.js
@@ -3,34 +3,35 @@ const API_ENDPOINT = process.env.endpoint;
 
 exports.handler = async (event, context) => {
 
-const sfsqlReqPayload=`[
-	{
-		"query": {
-			"sfsql": "SELECT $s:.Node.id as id, $s:.Node.label as label where  $s:.Node.type='character'"
-		},
-		"_comment": "nodes"
-	},
-	{
-		"query": {
-			"sfsql": "SELECT $o:.Node.edge.oid() as id, $s:.Node.id as source, $s:.Node.edge.Connection.id as target where $s:.Node.edge.Connection.type='character'"
-		},
-		"_comment": "edges"
-	}
-]`;
-	
-  return fetch(API_ENDPOINT, {
-	  headers: {
-		   "content-type": "application/json",
-         "x-sfsql-apikey": process.env.api_key
-		  }, 
-		  method: "POST",
-        body: sfsqlReqPayload,
-	  })
-    .then((response) => response.json())
-    .then((data) => ({
-		let nodes = data[0]['data'];
-      statusCode: 200,
-      body:JSON.stringify(nodes),
-    }))
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+        const sfsqlReqPayload=`[
+												{
+													"query": {
+														"sfsql": "SELECT $s:.Node.id as id, $s:.Node.label as label where  $s:.Node.type='character'"
+													},
+													"_comment": "nodes"
+												},
+												{
+													"query": {
+														"sfsql": "SELECT $o:.Node.edge.oid() as id, $s:.Node.id as source, $s:.Node.edge.Connection.id as target where $s:.Node.edge.Connection.type='character'"
+													},
+													"_comment": "edges"
+												}
+											]`
+        const body = sfsqlReqPayload
+        const init = {
+          body: body,
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            'x-sfsql-apikey': process.env.api_key
+          },
+        };
+        const response = await fetch(API_ENDPOINT, init);
+        const data =  await response.json();
+		  
+		  return {
+				statusCode: 200,
+				body: JSON.stringify(data),
+			 }; 
+		  .catch((error) => ({ statusCode: 422, body: String(error) }));
 };	 
